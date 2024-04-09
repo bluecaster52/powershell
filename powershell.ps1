cba7b934f697091a98d0003198cd45b2e896430b
bluecaster52 <166434846+bluecaster52@users.noreply.github.com>
2024-04-09 17:27:43 -0400

    Update powershell.ps1

--- powershell.ps1
+++ powershell.ps1
@@ -2,13 +2,13 @@
 # Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force -scope currentuser
 # Install module
 Install-Module -Name MouseSettings -force -Scope currentuser
-pause
+# pause
 # Get current settigs and pipe into file. 
 get-mousesettings | convertto-json | out-file c:\temp\mousesettings.json
-pause
+# pause
 # Change mouse primary button to be the right.
 set-mousesettings -PrimaryButton Right
-pause
+# pause
 # Change mouse settings
 set-mousesettings -ScrollTargetBehavior Legacy 
 set-mousesettings -Speed 1
@@ -19,9 +19,9 @@ set-mousesettings -VerticalScrollLineCount 40
 set-mousesettings -ClickLockEnabled 
 set-mousesettings -ClickLockDelay 110
 set-mousesettings -CursorSize 500
-pause
+# pause
 # Change windows theme
-start "" "C:\Windows\Resources\Ease of Access Themes\hcwhite.theme"
+# start "" "C:\Windows\Resources\Ease of Access Themes\hcwhite.theme"
 # start "" "C:\Windows\Resources\Ease of Access Themes\hcwhite.theme" & timeout /t 3 & taskkill /im "systemsettings.exe" /
 
 pause
