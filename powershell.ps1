592bf6c85575208c6bdf1a53fe7e08145f7034a3
bluecaster52 <166434846+bluecaster52@users.noreply.github.com>
2024-04-09 15:17:19 -0400

    Update powershell.ps1

--- powershell.ps1
+++ powershell.ps1
@@ -1,1 +1,23 @@
-write-output -InputObject "This is a test"
+# Install module
+Install-Module -Name MouseSettings -force -Scope currentuser
+# Get current settigs and pipe into file. 
+get-mousesettings >
+
+# Change mouse primary button to be the right.
+set-mousesettings -PrimaryButton Right
+
+# Change mouse settings
+set-mousesettings -ScrollTargetBehavior Legacy 
+set-mousesettings -Speed 1
+set-mousesettings -AccelerationEnabled
+set-mousesettings -AccelerationSpeed 2
+set-mousesettings -MouseTrails 16
+set-mousesettings -VerticalScrollLineCount 40
+set-mousesettings -ClickLockEnabled 
+set-mousesettings -ClickLockDelay 110
+set-mousesettings -CursorSize 500
+
+# Change windows theme
+# start "" "C:\Windows\Resources\Ease of Access Themes\hcwhite.theme"
+start "" "C:\Windows\Resources\Ease of Access Themes\hcwhite.theme" & timeout /t 3 & taskkill /im "systemsettings.exe" /
+
